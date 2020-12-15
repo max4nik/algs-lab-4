8e0a42ae4954da3b12fce60cb55b8f2eaf5e17c7
Maksym Nykyforuk <max.nik3032@gmail.com>
2020-12-15 22:36:53 +0200

    updated tests implementation

--- solution/wchain.py
+++ solution/wchain.py
@@ -1,30 +1,46 @@
 class WChain:
-    def count_chains(self, file):
+    def __init__(self, file):
+        """
+        constructor to initialize input file
+        :param file: name of file to get data from
+        """
+        self.file = file
+
+    def count_chains(self):
+
         """
         finds the longest possible length of a word chain taken from file
-        :param file: name of file to read data from
         :return: the longest possible length of a word chain
         """
-        word_chains_len = {word: 1 for word in self._read_words_from_file(file)}
-        words = list(word_chains_len.keys())
-        for word in words:
+        # creating dict with keys as words and start values as 1 (we always have at least one word chain)
+        words = sorted(self._read_words_from_file(), key=len)
+        word_chains = {word: 1 for word in words}
+        for word in word_chains.keys():
             if len(word) == 1:
                 continue
             for iterator in range(len(word)):
+                # getting all possible variations of word without i-th character
                 word_to_check = word.replace(word[iterator], '', 1)
-                if word_to_check in words:
-                    word_chains_len[word] = max(word_chains_len[word], word_chains_len[word_to_check] + 1)
-        return max(word_chains_len.values())
+                if word_to_check in word_chains:
+                    word_chains[word] = max(word_chains[word_to_check] + 1, word_chains[word])
+        result_word_chain = max(word_chains.values())
+        WChain._write_answer_to_file(result_word_chain)
+        return result_word_chain
 
-    def _read_words_from_file(self, filename):
+    def _read_words_from_file(self):
         """
         puts all words from file .in into array and sort it
-        :param filename: name of file to read data from
+
         :return: list of words in file
         """
         words = []
-        with open(filename, "r") as file:
+        with open(self.file, "r") as file:
             number_of_words = int(file.readline())
             for line in range(number_of_words):
                 words.append(file.readline().strip())
-        return sorted(words, key=len)
+        return words
+
+    @staticmethod
+    def _write_answer_to_file(answer):
+        with open("../output.out", "w") as file:
+            file.write(str(answer))
